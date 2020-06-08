64fbb70a1f0b92f5ba90544809a7272212010c3b
Xavi <xavivzla.g@gmail.com>
2020-06-07 22:19:08 -0500

    [UPDATE] - add events animations

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+interface PropsNotify {
+  message: string
+  time: number
+  type: string
+}
+
 class Notify {
   container: HTMLElement | undefined = undefined
   divNotification: HTMLElement | undefined = undefined
-  index: Number = 1
+  index: number = 1
   arr: string[] = []
   constructor() {
     if (typeof window !== 'undefined') {
@@ -81,6 +87,75 @@ class Notify {
     })
 
     return divNotification
+  }
+
+  setNotify(data: PropsNotify) {
+    let bg = '#07bc0c'
+    switch (data.type) {
+      case 'warning':
+        bg = '#F09200'
+        break
+      case 'error':
+        bg = '#FF322C'
+        break
+      default:
+        bg = '#13BF5F'
+        break
+    }
+    const item = Object.assign(document.createElement('div'), {
+      className: 'notifyCustom',
+      id: `notify-${this.index}`,
+      innerHTML: data.message,
+      style: `background-color: ${bg}`
+    })
+
+    return item
+  }
+
+  updateIndex() {
+    this.index += 1
+  }
+
+  // animations
+  animateOut(id: number) {
+    const target: HTMLElement | null  = document.getElementById(`notify-${id}`)
+
+    if(target) {
+      const targetAnimation = target.animate([
+        { opacity: '1' },
+        { opacity: '0' }
+      ], {
+        duration: 500
+      })
+
+      if(targetAnimation) {
+        targetAnimation.addEventListener('finish', () => {
+          target.style.opacity = '0'
+          target.remove()
+        })
+      }
+    }
+  }
+
+  animateIn() {
+    const target: HTMLElement | null = document.getElementById(`notify-${this.index}`)
+
+    if(target) {
+      const targetAnimation = target.animate([
+        { opacity: '0' },
+        { opacity: '.9' }
+      ], {
+        duration: 500,
+        id: `notify${this.index}`
+      })
+
+      if(targetAnimation) {
+        targetAnimation.addEventListener('finish', () => {
+          target.style.opacity = '1'
+        })
+        this.updateIndex()
+      }
+    }
   }
 }
 
